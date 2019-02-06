e5f99fa7dd4005e49fcb3b98540bdad1bd43687f
Tomoaki mbp13r-1 <1668339+TomoakiNagahara@users.noreply.github.com>
2019-02-07 02:31:51 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * unit-notfound:/index.php
+ *
+ * @creation  2019-01-29
+ * @version   1.0
+ * @package   unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('NotFound.class.php');
+include('Common.class.php');
+
+//	...
+return true;
