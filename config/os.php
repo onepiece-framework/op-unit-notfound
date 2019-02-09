c4dd4c6e0087f546146a6aa4ae38a98748ea4fc2
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-02-09 17:04:32 +0900

    New: config/os.php

---
+++ config/os.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * unit-notfound:/config/os.php
+ *
+ * Use from NotFound::_OS()
+ *
+ * @creation  2019-02-08
+ * @version   1.0
+ * @package   unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+$configs = [];
+
+//	...
+$configs['mac'] = 'Mac OS X (\d+)[_\.](\d+)';
+
+//	...
+return $configs;
