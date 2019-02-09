abfaf7d4a6396aca957172168032b7ea5144f9eb
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-02-09 17:04:44 +0900

    New: config/browser.php

---
+++ config/browser.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * unit-notfound:/config/browser.php
+ *
+ * Use from NotFound::_browser()
+ *
+ * @creation  2019-02-08
+ * @version   1.0
+ * @package   unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+$configs = [];
+
+//	...
+$configs['vivaldi'] = 'Vivaldi\/(\d+)\.(\d+)';
+$configs['opera']   = 'OPR\/(\d+)\.(\d+)';
+$configs['chrome']  = 'Chrome\/(\d+)\.(\d+)';
+$configs['safari']  = 'Safari\/(\d+)\.(\d+)';
+$configs['firefox'] = 'Firefox\/(\d+)\.(\d+)';
+
+//	...
+return $configs;
