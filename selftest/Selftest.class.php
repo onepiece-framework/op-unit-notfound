3ecd863786e20401e1a337bd4e37f98cd3d3d026
Tomoaki mbp13r-1 <tomoaki.nagahara@gmail.com>
2019-02-12 22:48:13 +0900

    New: selftest/Selftest.class.php

---
+++ selftest/Selftest.class.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * unit-notfound:/Selftest.class.php
+ *
+ * @creation  2019-02-04
+ * @version   1.0
+ * @package   unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ * @creation  2019-02-04
+ */
+namespace OP\UNIT\NOTFOUND;
+
+/** Selftest
+ *
+ * @creation  2019-02-04
+ * @version   1.0
+ * @package   unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Selftest implements \IF_UNIT
+{
+	/** trait.
+	 *
+	 */
+	use \OP_CORE;
+
+	/** Will execute automatically of Selftest.
+	 *
+	 */
+	static function Auto()
+	{
+		//	...
+		if(!\Unit::Load('selftest') ){
+			return;
+		};
+
+		/* @var $selftest \OP\UNIT\Selftest */
+		if( $io = $selftest = \Unit::Instantiate('Selftest') ){
+			$io = $selftest->Auto(__DIR__.'/../selftest/config.php');
+		};
+
+		//	...
+		if( $config = $_GET['debug'] ?? null ){
+			$selftest->Debug($config);
+		};
+
+		//	...
+		return $io;
+	}
+
+	/** For developers.
+	 *
+	 *
+	 * @see \IF_UNIT::Help()
+	 * @param	 string		 $topic
+	 */
+	function Help($topic=null)
+	{
+		echo '<pre><code>';
+		echo file_get_contents(__DIR__.'/README.md');
+		echo '</code></pre>';
+	}
+
+	/** For developers.
+	 *
+	 * @see \IF_UNIT::Debug()
+	 * @param	 string		 $topic
+	 */
+	function Debug($topic=null, $selftest=null)
+	{
+
+	}
+}
