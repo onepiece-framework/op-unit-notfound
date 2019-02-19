579feda274083d3e787788cd9a73c3cff6f0c4a4
Tomoaki iMac5K-1 <1668339+TomoakiNagahara@users.noreply.github.com>
2019-02-19 11:30:13 +0900

    Add: selftest/t_ua_browser.php - googlebot

--- selftest/t_ua_browser.php
+++ selftest/t_ua_browser.php
@@ -40,7 +40,7 @@ $configer->Set('column', [
 $configer->Set('column', [
 	'name'    => 'browser',
 	'type'    => 'enum',
-	'length'  => 'ie, edge, chrome, firefox, safari, opera, vivaldi',
+	'length'  => 'ie, edge, chrome, firefox, safari, opera, vivaldi, googlebot',
 	'null'    =>  true,
 	'collate' => 'ascii_general_ci',
 	'comment' => 'browser name. Unknown browser is null.',
