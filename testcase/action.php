5a4a72826ffb1f9a5932be3d6599c4ad13ba9c4c
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-08-24 17:08:44 +0900

    New: testcase > action.php

---
+++ testcase/action.php
@@ -0,0 +1,17 @@
+<?php
+/** op-unit-notfound:/testcase/action.php
+ *
+ * @created   2020-08-23
+ * @version   1.0
+ * @package   op-unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+D( Config::Get('notfound') );
