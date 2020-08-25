14214218ef331382c1a899028bd8d97fa706fd72
Tomoaki mbp13r-2015 <1668339+TomoakiNagahara@users.noreply.github.com>
2020-08-25 18:01:38 +0900

    New: admin > action.php

---
+++ admin/action.php
@@ -0,0 +1,17 @@
+<?php
+/** op-unit-notfound:/admin/action.php
+ *
+ * @created   2020-08-25
+ * @version   1.0
+ * @package   op-unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+Unit('NotFound')->Admin();
