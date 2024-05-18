9cfad42d3e8fa6efb07b652bacf59b8f61491fc5
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-05-19 06:33:55 +0900

    New: NotFound.class.php

---
+++ NotFound.class.php
@@ -0,0 +1,38 @@
+<?php
+/** op-unit-notfound:/NotFound.class.php
+ *
+ * @created    2024-05-18
+ * @version    1.0
+ * @package    op-unit-notfound
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+use OP\OP_CI;
+use OP\OP_CORE;
+use OP\OP_UNIT;
+use OP\IF_UNIT;
+
+/** NotFound
+ *
+ * @created    2024-05-18
+ * @version   1.0
+ * @package   op-unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class NotFound implements IF_UNIT
+{
+	/** trait.
+	 *
+	 */
+	use OP_CORE, OP_UNIT, OP_CI;
+}
