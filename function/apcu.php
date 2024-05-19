0fafcbe863e627f4ebc5a4239941eb8eb32db0b3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-19 10:36:24 +0900

    New: function > apcu.php

---
+++ function/apcu.php
@@ -0,0 +1,31 @@
+<?php
+/** op-unit-notfound:/function/apcu.php
+ *
+ * @created    2024-05-18
+ * @version    1.0
+ * @package    op-unit-notfound
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\NOTFOUND;
+
+/** use
+ *
+ */
+
+/** NotFound
+ *
+ * @created    2024-05-18
+ * @version   1.0
+ * @package   op-unit-notfound
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+function apcu()
+{
+	D();
+}
