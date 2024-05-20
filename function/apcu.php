a39a2cfb4c39ff1f71a7f7c3c1a9be6f278a33de
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-20 12:00:51 +0900

    Fix: function > apcu.php | Store path and count.

--- function/apcu.php
+++ function/apcu.php
@@ -27,5 +27,34 @@ namespace OP\UNIT\NOTFOUND;
  */
 function apcu()
 {
-	D();
+	//	...
+	if(!apcu_enabled() ){
+		op()->Notice("APCu is disabled.");
+		return;
+	}
+
+	//	...
+	$apcu_key = 'op-unit-notfound';
+	$parsed   = OP()->ParseURL($_SERVER['REQUEST_URI']);
+	$path     = $parsed['path'];
+	$path_key = md5($path);
+	$path_key = substr($path_key, 0, 10);
+
+	//	...
+	if(!$apcu = apcu_fetch($apcu_key) ){
+		$apcu = [];
+	}
+
+	//	...
+	if( empty($apcu[$path_key]) ){
+		$apcu[$path_key] = [
+			'path'  => $path,
+			'count' => 0,
+		];
+	}
+	//	...
+	$apcu[$path_key]['count']++;
+
+	//	...
+	apcu_store($apcu_key, $apcu);
 }
